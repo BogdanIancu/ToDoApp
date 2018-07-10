263121703de9c7e92bdbb6c0f712b0a285ba16cb
Bogdan Iancu <bogdan.iancu@ymail.com>
2018-07-10 18:04:50 +0300

    Added implicit Intent to Calendar app

--- app/src/main/java/ro/liis/todoapp/AddTaskActivity.java
+++ app/src/main/java/ro/liis/todoapp/AddTaskActivity.java
@@ -1,7 +1,16 @@
 package ro.liis.todoapp;
 
+import android.content.Intent;
+import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class AddTaskActivity extends AppCompatActivity {
 
@@ -9,5 +18,24 @@ public class AddTaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+    }
+
+    public void AddTaskToCalendar(View view) {
+        Intent intent = new Intent(Intent.ACTION_INSERT,
+                CalendarContract.Events.CONTENT_URI);
+        Calendar calendar = Calendar.getInstance();
+        EditText dueDateEditTxt = findViewById(R.id.dueDateEditText);
+        String dueDate = dueDateEditTxt.getText().toString();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            Date date = format.parse(dueDate);
+            calendar.setTime(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        EditText titleEditTxt = findViewById(R.id.titleEditText);
+        intent.putExtra(CalendarContract.Events.TITLE, titleEditTxt.getText().toString());
+        intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, calendar.getTimeInMillis());
+        startActivity(intent);
     }
 }
