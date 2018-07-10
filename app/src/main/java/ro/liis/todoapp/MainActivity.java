eba8fbbcdf7720705c6250e2fbc79c8320d8af5e
Bogdan Iancu <bogdan.iancu@ymail.com>
2018-07-10 10:58:46 +0300

    Added onClick for Login

--- app/src/main/java/ro/liis/todoapp/MainActivity.java
+++ app/src/main/java/ro/liis/todoapp/MainActivity.java
@@ -3,6 +3,8 @@ package ro.liis.todoapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,8 +14,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.e("MainActivity", "Aplicatia a pornit!");
-        Toast t = Toast.makeText(getApplicationContext(), "A pornit!",
-                Toast.LENGTH_LONG);
-        t.show();
+    }
+
+    public void Login(View view){
+        EditText userEditTxt = findViewById(R.id.userEditText);
+        EditText passEditTxt = findViewById(R.id.passwordEditText);
+        if(userEditTxt.getText().toString().trim().length() > 4 &&
+                passEditTxt.getText().toString().trim().length() > 4) {
+
+        }
+        else {
+            Toast.makeText(getApplicationContext(),
+                    R.string.login_error,
+                    Toast.LENGTH_LONG).show();
+        }
     }
 }
