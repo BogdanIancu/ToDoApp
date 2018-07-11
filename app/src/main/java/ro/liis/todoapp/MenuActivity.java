df9e99ad0b07f9056606147d57c54baaad2e3c5d
Bogdan Iancu <bogdan.iancu@ymail.com>
2018-07-11 13:19:47 +0300

    Added ListView and Adapter

--- app/src/main/java/ro/liis/todoapp/MenuActivity.java
+++ app/src/main/java/ro/liis/todoapp/MenuActivity.java
@@ -5,10 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.Vector;
+
+import ro.liis.todoapp.model.Task;
 import ro.liis.todoapp.model.User;
 
 public class MenuActivity extends AppCompatActivity {
     public static User currentUser = null;
+    private ArrayList<Task> taskVector = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,21 @@ public class MenuActivity extends AppCompatActivity {
 
     public void goToAddTask(View view) {
         Intent intent = new Intent(MenuActivity.this, AddTaskActivity.class);
+        startActivityForResult(intent, 100);
+    }
+
+    public void goToTaskList(View view) {
+        Intent intent = new Intent(MenuActivity.this,
+                TaskListActivity.class);
+        intent.putExtra("list", taskVector);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == 100 && resultCode == RESULT_OK) {
+            Task t = (Task)data.getSerializableExtra("task");
+            taskVector.add(t);
+        }
     }
 }
---
+++ app/src/main/java/ro/liis/todoapp/TaskListActivity.java
@@ -0,0 +1,29 @@
+package ro.liis.todoapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Vector;
+
+import ro.liis.todoapp.adapters.TaskAdapter;
+import ro.liis.todoapp.model.Task;
+
+public class TaskListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_list);
+
+        Intent intent = getIntent();
+        ArrayList<Task> vector =
+                (ArrayList<Task>)intent.getSerializableExtra("list");
+        TaskAdapter adapter = new TaskAdapter(getApplicationContext(),
+                R.layout.task_item, vector);
+        ListView listView = findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+    }
+}
